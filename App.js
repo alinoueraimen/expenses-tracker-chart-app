90c1af0eb3653049d4178d3bd3a24868dbc052b9
alinoueraimen <alinuriman.dev@gmail.com>
2025-03-31 21:12:32 +0700

    adding some changes and testing supabase auth

--- App.js
+++ App.js
@@ -3,11 +3,22 @@ import { StatusBar } from "expo-status-bar";
 import ContextProviderWrapper from "./utils/ContextProviderWrapper";
 import { useEffect } from "react";
 import Constant from 'expo-constants'
+import useAuth from "./services/supabase/auth/useAuth";
 export default function App() {
-  const {dev} =Constant.expoConfig.extra
+  const {handleAuthListener,handleRetrieveUserSession}  = useAuth();
+  const authInitialize = async () => {
+    console.log("initializing auth")
+    const session = await handleRetrieveUserSession();
+    console.log(session);
+    return session
+  }
+  const {dev} =Constant.expoConfig.extra;
 useEffect(()=>{
   console.log(`we are in ${dev} mode`)
-},[])
+  authInitialize().then((result)=>{
+    console.log("auth initialize :",result);
+  })
+})
   return (
   <>
        <StatusBar style="auto" />
