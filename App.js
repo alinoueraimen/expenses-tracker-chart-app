35bac15227c4add1e2feba65d1f94c4d6f663af0
alinoueraimen <alinuriman.dev@gmail.com>
2025-04-03 16:18:20 +0700

    feat(saveTransaction) : now user can save transaction into the server

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import AppNavigator from "./navigation/AppNavigator";
 import { StatusBar } from "expo-status-bar";
 import ContextProviderWrapper from "./utils/ContextProviderWrapper";
+import { useEffect } from "react";
 
 export default function App() {
-  
+  useEffect(()=>{
+    console.log("app component");
+  })
   return (
   <>
        <StatusBar style="auto" />
